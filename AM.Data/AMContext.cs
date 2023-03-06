2eeb8abed84af40ad1b8752a1540257a2226dde6
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2023-03-06 12:19:58 +0100

    Création de la couche Data

---
+++ AM.Data/AMContext.cs
@@ -0,0 +1,24 @@
+﻿
+using AM.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AM.Data
+{
+    public class AMContext : DbContext
+    {
+        //Parametre l'acces a la BD
+
+        public DbSet<Passenger> passenger { get; set; }
+        public DbSet<Flight> Flight { get; set; }
+        public DbSet<Plane> Plane { get; set; }
+        public DbSet<Staff> Staff { get; set; }
+        public DbSet<Traveller> travellers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;
+                        Initial Catalog = Airport;  
+                        Integrated Security = true");
+
+        }
+    }
+}
