4c8245a92cda20336c678812cfc4dbb7d0037075
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2023-04-10 14:41:26 +0100

    Update-DataBase

--- AM.Data/Migrations/20230410133823_TPT.Designer.cs
+++ AM.Data/Migrations/20230410133823_TPT.Designer.cs
@@ -4,6 +4,7 @@ using AM.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AM.Data.Migrations
 {
     [DbContext(typeof(AMContext))]
-    partial class AMContextModelSnapshot : ModelSnapshot
+    [Migration("20230410133823_TPT")]
+    partial class TPT
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AM.Data/Migrations/20230410133823_TPT.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AM.Data.Migrations
+{
+    public partial class TPT : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmployementDate",
+                table: "Passengers");
+
+            migrationBuilder.DropColumn(
+                name: "Function",
+                table: "Passengers");
+
+            migrationBuilder.DropColumn(
+                name: "HealthInformation",
+                table: "Passengers");
+
+            migrationBuilder.DropColumn(
+                name: "Nationality",
+                table: "Passengers");
+
+            migrationBuilder.DropColumn(
+                name: "Salary",
+                table: "Passengers");
+
+            migrationBuilder.DropColumn(
+                name: "isTraveler",
+                table: "Passengers");
+
+            migrationBuilder.CreateTable(
+                name: "Staffs",
+                columns: table => new
+                {
+                    PassportNumber = table.Column<string>(type: "nvarchar(7)", maxLength: 7, nullable: false),
+                    EmployementDate = table.Column<DateTime>(type: "date", nullable: false),
+                    Function = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Salary = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Staffs", x => x.PassportNumber);
+                    table.ForeignKey(
+                        name: "FK_Staffs_Passengers_PassportNumber",
+                        column: x => x.PassportNumber,
+                        principalTable: "Passengers",
+                        principalColumn: "PassportNumber");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Travellers",
+                columns: table => new
+                {
+                    PassportNumber = table.Column<string>(type: "nvarchar(7)", maxLength: 7, nullable: false),
+                    HealthInformation = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nationality = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Travellers", x => x.PassportNumber);
+                    table.ForeignKey(
+                        name: "FK_Travellers_Passengers_PassportNumber",
+                        column: x => x.PassportNumber,
+                        principalTable: "Passengers",
+                        principalColumn: "PassportNumber");
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Staffs");
+
+            migrationBuilder.DropTable(
+                name: "Travellers");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EmployementDate",
+                table: "Passengers",
+                type: "date",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Function",
+                table: "Passengers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HealthInformation",
+                table: "Passengers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nationality",
+                table: "Passengers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Salary",
+                table: "Passengers",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "isTraveler",
+                table: "Passengers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
